88f3ac4ae56784708042a81de907d12c1d2d2795
isaac okyere <122814014+OkyereI@users.noreply.github.com>
2023-10-16 09:45:07 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -9,13 +9,15 @@ class User(BaseModel):
     """Represent a User
 
     Attributes:
-        email (str): user email
-        password (str): user password
+    
         first_name (str): first name
         last_name (str): last name
+        email (str): user email
+        password (str): user password
+       
 
     """
-    email = ""
-    password = ""
     first_name = ""
     last_name = ""
+    email = ""
+    password = ""
